8a3c8219f7ec1218fc889b3984ff3fdc5dc75fce
Rashid <rashid.latif93@gmail.com>
2019-01-13 01:01:53 +0500

    Initial Commit

---
+++ FlotingButton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlotingButton
+//
+//  Created by Rashid on 13/01/2019.
+//  Copyright © 2019 Rashid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
